808aa6454f5fb058f6a9a531f89de0bc14651b57
Hiroshi Onozaki <hrs-o@users.noreply.github.com>
2021-11-28 15:55:57 +0900

    キーボードによるブロックの回転処理

---
+++ Assets/BlockTower/Scripts/Views/Main/Block/BlockRotator.cs
@@ -0,0 +1,58 @@
+﻿using UniRx;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace BlockTower.Views.Main.Block
+{
+    public class BlockRotator : MonoBehaviour
+    {
+        [SerializeField] private float rotateAngle = 30f;
+
+        private InputAction _inputRotateLeft;
+        private InputAction _inputRotateRight;
+
+        private float _startY;
+        private Rigidbody2D _rigidbody;
+
+        private readonly CompositeDisposable _disposables = new();
+
+        private void Start()
+        {
+            var playerInput = GetComponent<PlayerInput>();
+            var actionMap = playerInput.currentActionMap;
+            _inputRotateLeft = actionMap["RotateLeft"];
+            _inputRotateRight = actionMap["RotateRight"];
+
+            _rigidbody = GetComponent<Rigidbody2D>();
+            _startY = _rigidbody.position.y;
+
+            this.ObserveEveryValueChanged(_ => _inputRotateLeft.WasPressedThisFrame())
+                .Where(wasPressed => wasPressed)
+                .Subscribe(_ => RotateLeft())
+                .AddTo(this)
+                .AddTo(_disposables);
+
+            this.ObserveEveryValueChanged(_ => _inputRotateRight.WasPressedThisFrame())
+                .Where(wasPressed => wasPressed)
+                .Subscribe(_ => RotateRight())
+                .AddTo(this)
+                .AddTo(_disposables);
+
+            this.ObserveEveryValueChanged(_ => _rigidbody.position.y)
+                .Where(posY => posY < _startY)
+                .Subscribe(_ => _disposables.Dispose())
+                .AddTo(this)
+                .AddTo(_disposables);
+        }
+
+        private void RotateLeft()
+        {
+            transform.Rotate(0, 0, rotateAngle);
+        }
+
+        private void RotateRight()
+        {
+            transform.Rotate(0, 0, -rotateAngle);
+        }
+    }
+}
